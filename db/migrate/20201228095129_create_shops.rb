2d20c9ce5a3211063ebd80eb256f7a3049493826
RyuseiNono <ryuno72444za@gmail.com>
2020-12-28 20:57:54 +0900

    ショップDB作成

---
+++ db/migrate/20201228095129_create_shops.rb
@@ -0,0 +1,17 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name,                    null: false
+      t.text :location,                  null: false
+      t.string :phone_number,            null: false
+      t.integer :prefecture_id
+      t.time :opening_hours
+      t.time :closing_time
+      t.integer :parking_id
+      t.integer :credit_card_id
+      t.integer :electronic_money_id
+      t.references :admin,               null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
