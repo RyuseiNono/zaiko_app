e9d4f8e97fed9f475d38fd2c982a57944a58d4a2
RyuseiNono <ryuno72444za@gmail.com>
2021-01-03 13:07:16 +0900

    timeの表示修正

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -13,7 +13,6 @@ class ShopsController < ApplicationController
   end
 
   def create
-    binding.pry
     @shop = Shop.new(shop_params)
     render :new and return if params[:back] || !@shop.save
   end
@@ -22,23 +21,11 @@ class ShopsController < ApplicationController
 
   def shop_params
     shop_params = params.require(:shop).permit(:name, :prefecture_id, :location, :phone_number, \
-                                               :'opening_time(4i)', :'opening_time(5i)', \
-                                               :'closing_time(4i)', :'closing_time(5i)', \
                                                :parking_id, :credit_card_id, :electronic_money_id, \
                                                :opening_time, :closing_time)\
                         .merge(admin_id: current_admin[:id])
     # 全角とハイフンの処理
     shop_params[:phone_number] = shop_params[:phone_number].tr('０-９ａ-ｚＡ-Ｚー', '0-9a-zA-Z-').gsub(/[−-]/, '')
-    # 開店時間、閉店時間の処理
-    if action_name == 'confirm'
-      shop_params[:opening_time] = "#{shop_params['opening_time(4i)']}:#{shop_params['opening_time(5i)']}:00"
-      shop_params[:closing_time] = "#{shop_params['closing_time(4i)']}:#{shop_params['closing_time(5i)']}:00"
-    end
-    #無駄な項目を削除
-    delete_keys=[:'opening_time(1i)',:'opening_time(2i)',:'opening_time(3i)',:'opening_time(4i)',:'opening_time(5i)',:'closing_time(1i)',:'closing_time(2i)',:'closing_time(3i)',:'closing_time(4i)',:'closing_time(5i)']
-    delete_keys.each do |key|
-      shop_params.delete(key)
-    end
-    return shop_params
+    shop_params
   end
 end
