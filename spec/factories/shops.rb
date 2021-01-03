465c5b20c3b13402388e4f512f5cecbbf2f4d01a
RyuseiNono <ryuno72444za@gmail.com>
2021-01-03 21:42:29 +0900

    緒修正

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     name                     { "#{Faker::Address.city}店" }
     location                 { Faker::Address.state }
     phone_number             { Faker::PhoneNumber.phone_number.gsub(/-/, '') }
-    prefecture_id            { rand(2..Prefecture.all.length+1) }
+    prefecture_id            { rand(1..Prefecture.all.length) }
     opening_time             { '10:00:00' }
     closing_time             { '20:00:00' }
-    parking_id               { rand(1..Parking.all.length)}
+    parking_id               { rand(1..Parking.all.length) }
     credit_card_id           { rand(1..CreditCard.all.length) }
     electronic_money_id      { rand(1..ElectronicMoney.all.length) }
     association :admin
