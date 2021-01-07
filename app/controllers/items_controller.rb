f747a20424ac461cefea2dc1d53998b196f1ca1e
RyuseiNono <ryuno72444za@gmail.com>
2021-01-08 02:07:40 +0900

    Actioncable実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,29 @@
 class ItemsController < ApplicationController
   def index
     @shop = Shop.find(params['shop_id'])
-    @items = @shop.items
+  end
+
+  def new
+    @shop = Shop.find(params['shop_id'])
+    @item = Item.new
+  end
+
+  def create
+    @shop = Shop.find(params['shop_id'])
+    @item = Item.new(item_params)
+    if @item.save
+      item_id = Item.order(id: "DESC").limit(1).ids[0] #今投稿したitemのid取得
+      items_length = @shop.items.length #データ数０か１以上で場合分けするため
+      ActionCable.server.broadcast 'item_channel', content: {item: @item, item_id: item_id ,items_length: items_length}
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :count).merge(shop_id: params[:shop_id])
+  end
+
+  def set_item
+    @shop = Shop.find(params['shop_id'])
   end
 end
