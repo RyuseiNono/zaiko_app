177d41a6f477c5adb53540686b40f71bfbebcb84
RyuseiNono <ryuno72444za@gmail.com>
2021-01-09 18:09:18 +0900

    在庫を増やした時のフロント崩れの更新

--- app/javascript/channels/item_create_channel.js
+++ app/javascript/channels/item_create_channel.js
@@ -10,47 +10,68 @@ consumer.subscriptions.create("ItemCreateChannel", {
   },
 
   received(data) {
-    // debugger;
+    const item = data.content.item;
     // 要素取得
     const items = document.getElementById("items");
+    const clone_new = document.getElementById("item-#clone_new");
+    const clone_index = document.getElementById("item-#clone_index");
     const items_table = document.getElementById("items_table");
     const items_empty = document.getElementById("items_empty");
-    const add_item_name = document.getElementById("add_item_name");
-    // 追加する要素の素材を作成
-    var tr = document.createElement("tr");
-    var td_name = document.createElement("td");
-    var td_count = document.createElement("td");
-    var td_Button = document.createElement("td");
-    // id追加
-    tr.id = `item-#${data.content.item.id}`;
-    // createElementに商品名、在庫数を代入
-    td_count.textContent = data.content.item.count;
-    if (data.content.item.count >= 1) {
-      td_name.textContent = data.content.item.name;
-    } else if (data.content.item.count == 0) {
-      const del = document.createElement("del");
-      del.textContent = data.content.item.name;
-      td_name.appendChild(del);
-      td_name.className = "text-muted";
-      td_count.className = "text-muted";
+    // new
+    if (clone_new != null) {
+      // ノード定義
+      var new_item = clone_new.cloneNode(true);
+      var new_item_tr = new_item;
+      var new_item_form = new_item.getElementsByTagName("form")[0];
+      var new_item_input_name = new_item.getElementsByTagName("input")[1];
+      var new_item_input_count = new_item.getElementsByTagName("input")[2];
+      var new_item_button_update = new_item.getElementsByTagName("button")[0];
+      var new_item_button_delete = new_item.getElementsByTagName("a")[0];
+
+      // フォーム構築
+      new_item.hidden = false;
+      new_item_tr.id = `item-#${item.id}`;
+      new_item_form.id = `form-#${item.id}`;
+      new_item_form.action = `/shops/${item.shop_id}/items/${item.id}`;
+      new_item_input_name.value = item.name;
+      new_item_input_name.id = `item_name_new-#${item.id}`;
+      new_item_input_name.setAttribute("form", `form-#${item.id}`);
+      new_item_input_name.name = "item[name]";
+      new_item_input_count.value = item.count;
+      new_item_input_count.id = `item_count_new-#${item.id}`;
+      new_item_input_count.setAttribute("form", `form-#${item.id}`);
+      new_item_input_count.name = "item[count]";
+      new_item_button_update.setAttribute("form", `form-#${item.id}`);
+      new_item_button_delete.href = `/shops/${item.shop_id}/items/${item.id}`;
+      items.appendChild(new_item);
+
+      // 入力欄消去
+      const add_item_name = document.getElementById("add_item_name");
+      add_item_name.value = "";
     }
-    const column = items.appendChild(tr); //itemsにtrを追加
-    column.appendChild(td_name); //追加したtrにtd_countを追加
-    column.appendChild(td_count); //追加したtrにtd_countを追加
-    // 以下ボタンの作成
-    td_Button.className = "w-25"; //td_Buttonにクラスを追加
-    const Button = column.appendChild(td_Button); //追加したtrにクラスを付与したtd_Buttonを追加
-    const addButton = document.createElement("a"); //aタグを作成
-    addButton.className = "btn btn-danger"; //addButtonにクラスを追加
-    addButton.textContent = "在庫を削除する"; //addButtonに要素を追加
-    addButton.href = `/shops/${data.content.item.shop_id}/items/${data.content.item.id}`; //addButtonにhrefを追加
-    addButton.dataset.confirm = "この作業は戻せません。本当に削除しますか?"; //addButtonにdata-confirmを追加
-    addButton.dataset.method = "delete"; //addButtonにdata-methodを追加
-    addButton.dataset.remote = "true"; //addButtonにdata-remoteを追加
-    // アイコン作成
-    const addButtonIcon = document.createElement("i");
-    addButtonIcon.className = "far fa-trash-alt mx-1";
-    Button.appendChild(addButton).appendChild(addButtonIcon);
+    // 　index
+    if (clone_index != null) {
+      // ノード定義
+      var new_item = clone_index.cloneNode(true);
+      var new_item_tr = new_item;
+      var new_item_name = new_item.getElementsByTagName("td")[0];
+      var new_item_count = new_item.getElementsByTagName("td")[1];
+
+      // 構築
+      new_item.hidden = false;
+      new_item_tr.id = `item-#${item.id}`;
+      new_item_name.textContent = item.name;
+      new_item_count.textContent = item.count;
+      items.appendChild(new_item);
+
+      // countが0の時の処理
+      if (item.count == 0) {
+        new_item_name.classList.add("text-muted");
+        new_item_count.classList.add("text-muted");
+      }
+    }
+
+    //itemの数による処理
     if (data.content.items_length == 0) {
       items_table.hidden = true;
       items_empty.hidden = false;
@@ -60,7 +81,69 @@ consumer.subscriptions.create("ItemCreateChannel", {
       // 追加された時に「商品はまだありません。」を消す
       items_empty.hidden = true;
     }
-    // 以下入力欄を空に
-    add_item_name.value = "";
+
+    // ボタン構築
+
+    // // 入力欄消去
+    // const add_item_name = document.getElementById("add_item_name");
+    // add_item_name.value = "";
+
+    // // debugger;
+    // // 要素取得
+    // const items = document.getElementById("items");
+    // const items_table = document.getElementById("items_table");
+    // const items_empty = document.getElementById("items_empty");
+    // const add_item_name = document.getElementById("add_item_name");
+    // // 追加する要素の素材を作成
+    // var tr = document.createElement("tr");
+    // var td_name = document.createElement("td");
+    // var input_name = document.createElement("input");
+    // var td_count = document.createElement("td");
+    // var input_count = document.createElement("input");
+    // var td_Button = document.createElement("td");
+    // // id追加
+    // tr.id = `item-#${data.content.item.id}`;
+    // // createElementに商品名、在庫数を代入
+    // input_count.value = data.content.item.count;
+    // td_count.appendChild(input_count);
+    // input_count.value = data.content.item.name;
+    // td_name.appendChild(input_name);
+    // // if (data.content.item.count >= 1) {
+    // //   td_name.textContent = data.content.item.name;
+    // // } else if (data.content.item.count == 0) {
+    // //   const del = document.createElement("del");
+    // //   del.textContent = data.content.item.name;
+    // //   td_name.appendChild(del);
+    // //   td_name.className = "text-muted";
+    // //   td_count.className = "text-muted";
+    // // }
+    // const column = items.appendChild(tr); //itemsにtrを追加
+    // column.appendChild(td_name); //追加したtrにtd_countを追加
+    // column.appendChild(td_count); //追加したtrにtd_countを追加
+    // // 以下ボタンの作成
+    // td_Button.className = "w-25"; //td_Buttonにクラスを追加
+    // const Button = column.appendChild(td_Button); //追加したtrにクラスを付与したtd_Buttonを追加
+    // const addButton = document.createElement("a"); //aタグを作成
+    // addButton.className = "btn btn-danger"; //addButtonにクラスを追加
+    // addButton.textContent = "在庫を削除する"; //addButtonに要素を追加
+    // addButton.href = `/shops/${data.content.item.shop_id}/items/${data.content.item.id}`; //addButtonにhrefを追加
+    // addButton.dataset.confirm = "この作業は戻せません。本当に削除しますか?"; //addButtonにdata-confirmを追加
+    // addButton.dataset.method = "delete"; //addButtonにdata-methodを追加
+    // addButton.dataset.remote = "true"; //addButtonにdata-remoteを追加
+    // // アイコン作成
+    // const addButtonIcon = document.createElement("i");
+    // addButtonIcon.className = "far fa-trash-alt mx-1";
+    // Button.appendChild(addButton).appendChild(addButtonIcon);
+    // if (data.content.items_length == 0) {
+    //   items_table.hidden = true;
+    //   items_empty.hidden = false;
+    // } else {
+    //   // itemの時、テーブルがhidddenであるため表示させる
+    //   items_table.hidden = false;
+    //   // 追加された時に「商品はまだありません。」を消す
+    //   items_empty.hidden = true;
+    // }
+    // // 以下入力欄を空に
+    // add_item_name.value = "";
   },
 });
--- app/javascript/channels/item_update_channel.js
+++ app/javascript/channels/item_update_channel.js
@@ -10,30 +10,39 @@ consumer.subscriptions.create("ItemUpdateChannel", {
   },
 
   received(data) {
-    var item_name_new = document.getElementById(
-      `item_name_new-#${data.content.item.id}`
-    );
-    var item_count_new = document.getElementById(
-      `item_count_new-#${data.content.item.id}`
-    );
-    var item_name_index = document.getElementById(
-      `item_name_index-#${data.content.item.id}`
-    );
-    var item_count_index = document.getElementById(
-      `item_count_index-#${data.content.item.id}`
-    );
+    const item = data.content.item;
+    const clone_new = document.getElementById("item-#clone_new");
+    const clone_index = document.getElementById("item-#clone_index");
+    const update_item = document.getElementById(`item-#${item.id}`);
 
-    if (item_name_new != null) {
-      item_name_new.value = `${data.content.item.name}`;
-    }
-    if (item_count_new != null) {
-      item_count_new.value = `${data.content.item.count}`;
-    }
-    if (item_name_index != null) {
-      item_name_index.textContent = data.content.item.name;
-    }
-    if (item_count_index != null) {
-      item_count_index.textContent = data.content.item.count;
+    if (clone_new != null) {
+      var item_name_new = document.getElementById(`item_name_new-#${item.id}`);
+      var item_count_new = document.getElementById(
+        `item_count_new-#${item.id}`
+      );
+
+      item_name_new.value = `${item.name}`;
+      item_count_new.value = `${item.count}`;
+    } else if (clone_index != null) {
+      var item_name_index = document.getElementById(
+        `item_name_index-#${item.id}`
+      );
+      var item_count_index = document.getElementById(
+        `item_count_index-#${item.id}`
+      );
+      item_name_index.textContent = item.name;
+      item_count_index.textContent = item.count;
+
+      // countによる処理
+      var new_item_name = update_item.getElementsByTagName("td")[0];
+      var new_item_count = update_item.getElementsByTagName("td")[1];
+      if (item.count == 0) {
+        new_item_name.classList.add("text-muted");
+        new_item_count.classList.add("text-muted");
+      } else {
+        new_item_name.classList.remve("text-muted");
+        new_item_count.classList.remve("text-muted");
+      }
     }
   },
 });
