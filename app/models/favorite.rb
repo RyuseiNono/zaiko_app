b98135847f5f1dd4e6ac315910a819f0a334a25e
RyuseiNono <ryuno72444za@gmail.com>
2021-01-14 19:52:34 +0900

    バリデーションリファクタリング

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,14 +3,8 @@ class Favorite < ApplicationRecord
   belongs_to :user,  required: false
   belongs_to :shop
 
-  validates_uniqueness_of :shop_id, scope: :user_id, if: :user_signed_in?
-  validates_uniqueness_of :shop_id, scope: :admin_id, if: :admin_signed_in?
+  validates_uniqueness_of :shop_id, scope: :user_id, if: :user_id
+  validates_uniqueness_of :shop_id, scope: :admin_id, if: :admin_id
 
-  def user_signed_in?
-    user_id != nil
-  end
 
-  def admin_signed_in?
-    admin_id != nil
-  end
 end
