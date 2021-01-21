6556202cdefe121052756c121fb061b4fca8b018
RyuseiNono <ryuno72444za@gmail.com>
2021-01-21 20:29:33 +0900

    Adminの結合テスト

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -1,33 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "Admins", type: :system do
   before do
-    @user = FactoryBot.build(:user)
+    @admin = FactoryBot.build(:admin)
+
   end
 
-  context '一般ユーザー新規登録ができるとき' do
-    it '正しい情報を入力すれば一般ユーザー新規登録ができてトップページに移動する' do
+  context '店舗ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すれば店舗ユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
       visit(root_path)
       # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録アカウント選択ページへ移動する
       visit(entrances_sessions_path)
-      # 一般ユーザー新規登録ページへ移動する
-      visit(new_user_registration_path)
-      # 一般ユーザー情報を入力する
-      fill_in('name', with: @user.name)
-      fill_in('email', with: @user.email)
-      fill_in('password', with: @user.password)
-      fill_in('password-confirmation', with: @user.password_confirmation)
+      # 店舗ユーザー新規登録ページへ移動する
+      visit(new_admin_registration_path)
+      # 店舗ユーザー情報を入力する
+      fill_in('name', with: @admin.name)
+      fill_in('email', with: @admin.email)
+      fill_in('password', with: @admin.password)
+      fill_in('password-confirmation', with: @admin.password_confirmation)
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
       expect{
-        find('button[id="user_registration_submit"]').click
-      }.to change { User.count }.by(1)
+        find('button[id="admin_registration_submit"]').click
+      }.to change { Admin.count }.by(1)
       # トップページへ遷移する
       expect(current_path).to eq root_path
       # ユーザーの名前とログアウトボタンが表示されていることを確認する
-      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@admin.name)
       expect(page).to have_content('ログアウト')
       # 簡単ログイン、新規登録、ログインのボタンが表示されていないことを確認する
       expect(page).to have_no_content('簡単ログイン')
@@ -36,27 +37,27 @@ RSpec.describe "Users", type: :system do
     end
   end
 
-  context '一般ユーザー新規登録ができないとき' do
-    it '誤った情報では一般ユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+  context '店舗ユーザー新規登録ができないとき' do
+    it '誤った情報では店舗ユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
       visit(root_path)
       # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録アカウント選択ページへ移動する
       visit(entrances_sessions_path)
-      # 一般ユーザー新規登録ページへ移動する
-      visit(new_user_registration_path)
-      # 一般ユーザー情報を入力する
+      # 店舗ユーザー新規登録ページへ移動する
+      visit(new_admin_registration_path)
+      # 店舗ユーザー情報を入力する
       fill_in('name', with: '')
       fill_in('email', with: '')
       fill_in('password', with: '')
       fill_in('password-confirmation', with: '')
-      # サインアップボタンを押してもuserモデルのカウントは上がらないことを確認する
+      # サインアップボタンを押してもadminモデルのカウントは上がらないことを確認する
       expect{
-        find('button[id="user_registration_submit"]').click
-      }.to change { User.count }.by(0)
+        find('button[id="admin_registration_submit"]').click
+      }.to change { Admin.count }.by(0)
       # 新規登録ページへ戻されることを確認する
-      expect(current_path).to eq "/users"
+      expect(current_path).to eq "/admins"
     end
   end
 end
