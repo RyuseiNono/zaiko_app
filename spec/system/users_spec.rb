55025c043e9e3694e0deb2a9ccc33fd897f0daac
RyuseiNono <ryuno72444za@gmail.com>
2021-01-22 01:44:25 +0900

    一般ユーザーログイン結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "User新規登録", type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
@@ -10,7 +10,7 @@ RSpec.describe "Users", type: :system do
       # トップページに移動する
       visit(root_path)
       # トップページにサインアップページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('新規登録')
+      expect(page).to have_link('新規登録')
       # 新規登録アカウント選択ページへ移動する
       visit(entrances_sessions_path)
       # 一般ユーザー新規登録ページへ移動する
@@ -27,12 +27,12 @@ RSpec.describe "Users", type: :system do
       # トップページへ遷移する
       expect(current_path).to eq root_path
       # ユーザーの名前とログアウトボタンが表示されていることを確認する
-      expect(page).to have_content(@user.name)
-      expect(page).to have_content('ログアウト')
+      expect(page).to have_link(@user.name)
+      expect(page).to have_link('ログアウト')
       # 簡単ログイン、新規登録、ログインのボタンが表示されていないことを確認する
-      expect(page).to have_no_content('簡単ログイン')
-      expect(page).to have_no_content('ログイン')
-      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_link('簡単ログイン')
+      expect(page).to have_no_link('ログイン')
+      expect(page).to have_no_link('新規登録')
     end
   end
 
@@ -41,14 +41,14 @@ RSpec.describe "Users", type: :system do
       # トップページに移動する
       visit(root_path)
       # トップページにサインアップページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('新規登録')
+      expect(page).to have_link('新規登録')
       # 新規登録アカウント選択ページへ移動する
       visit(entrances_sessions_path)
       # 一般ユーザー新規登録ページへ移動する
       visit(new_user_registration_path)
       # 一般ユーザー情報を入力する
       fill_in('name', with: '')
-      fill_in('email', with: '')
+      fill_in('email', with: 'a@a')
       fill_in('password', with: '')
       fill_in('password-confirmation', with: '')
       # サインアップボタンを押してもuserモデルのカウントは上がらないことを確認する
@@ -57,6 +57,62 @@ RSpec.describe "Users", type: :system do
       }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq "/users"
+    end
+  end
+end
+
+RSpec.describe "Userログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit(root_path)
+      # トップページにログインアカウント選択ページへ遷移するボタンがあることを確認する
+      expect(page).to have_link('ログイン')
+      # ログインアカウント選択ページへ遷移する
+      visit(entrances_sessions_path)
+      # 一般ユーザーログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_link('一般ユーザー')
+      # 一般ユーザー新規登録ページへ移動する
+      visit(new_user_session_path)
+      # 正しいユーザー情報を入力する
+      fill_in('email', with: @user.email)
+      fill_in('password', with: @user.password)
+      # ログインボタンを押す
+      find('button[id=submit_btn]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # ユーザーの名前とログアウトボタンが表示されていることを確認する
+      expect(page).to have_link(@user.name)
+      expect(page).to have_link('ログアウト')
+      # 簡単ログイン、新規登録、ログインのボタンが表示されていないことを確認する
+      expect(page).to have_no_link('簡単ログイン')
+      expect(page).to have_no_link('ログイン')
+      expect(page).to have_no_link('新規登録')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit(root_path)
+      # トップページにログインアカウント選択ページへ遷移するボタンがあることを確認する
+      expect(page).to have_link('ログイン')
+      # ログインアカウント選択ページへ遷移する
+      visit(entrances_sessions_path)
+      # 一般ユーザーログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_link('一般ユーザー')
+      # 一般ユーザー新規登録ページへ移動する
+      visit(new_user_session_path)
+      # 正しいユーザー情報を入力する
+      fill_in('email', with: 'a@a')
+      fill_in('password', with: '')
+      # ログインボタンを押す
+      find('button[id=submit_btn]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
     end
   end
 end
